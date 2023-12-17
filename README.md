1dd52b88512a0b16412aeb2015c8675120bf37df
Salahin <rocky.bgta@gmail.com>
2023-12-17 16:27:55 +0600

    finished task

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# brainstationTask23
+# spring-data-jpa
+required cmd
+docker exec -it kafka /bin/sh
+ls
+cd opt
+ls
+cd kafka_2.13-2.8.1
+pwd
+ls
+cd bin
+# To create topic =========
+kafka-topics.sh --create --zookeeper zookeeper:2181 --topic user-event --partitions 3 --replication-factor 1
+
+
+# To build Docker image give below cmd (salehinrocky docker user name):
+* docker build -t salehinrocky/brainstation-user-event:1.0.0 .
+# To run build image to give the following cmd:
+* docker run -p 8080:9191 salehinrocky/spring-boot-caching-redis:1.0.0
+# To give name of running container from an image give the following cmd: 
+* docker run -p 8080:9191 --name spring-boot-caching-redis salehinrocky/brainstation-user-event:1.0.0
+_Here salehinrocky/brainstation-user-event:1.0.0 is given image name_
+
+# To test api that run inside docker container
+* first try with http://localhost:8080/user
+
--- src/main/java/com/salahin/rocky/service/UserService.java
+++ src/main/java/com/salahin/rocky/service/UserService.java
@@ -37,69 +37,74 @@ public class UserService {
 
     @SneakyThrows
     @CacheEvict(cacheNames = "users", allEntries = true)
-    public UserDto saveUser(UserDto userDto){
+    public UserDto saveUser(UserDto userDto) {
         UserEntity userEntity;
         userEntity = modelMapper.map(userDto, UserEntity.class);
         userEntity = userRepository.save(userEntity);
         userDto = modelMapper.map(userEntity, UserDto.class);
         log.info("UsersService::saveUser() connecting to Database");
-        kafkaTemplate.send(topicName,new ObjectMapper().writeValueAsString(userDto));
+        kafkaTemplate.send(topicName, new ObjectMapper().writeValueAsString(userDto));
         log.info("Published save user event to kafka user-event topic");
         return userDto;
     }
 
-    @Cacheable(cacheNames = "users",sync = true)
+    @Cacheable(cacheNames = "users", sync = true)
     public List<UserDto> getAllUsers() {
         List<UserEntity> userEntityList;
+        List<UserDto> userDtoList=null;
         userEntityList = userRepository.findAll();
         log.info("UsersService::getAllUsers() connecting to Database");
-        List<UserDto> userDtoList = userEntityList.stream()
-                .map(user -> modelMapper.map(user, UserDto.class))
-                .collect(Collectors.toList());
+        if (userEntityList != null && userEntityList.size() > 0) {
+            userDtoList = userEntityList.stream()
+                    .map(user -> modelMapper.map(user, UserDto.class))
+                    .collect(Collectors.toList());
+        }
         return userDtoList;
     }
 
     @CacheEvict(cacheNames = "users", allEntries = true)
-    public UserDto getUsersById(int id){
+    public UserDto getUsersById(int id) {
         Optional<UserEntity> users;
         UserDto userDto = new UserDto();
         users = userRepository.findById(id);
         log.info("UsersService::getUsersById() connecting to Database");
-        if(users.isPresent()) {
+        if (users.isPresent()) {
             userDto = modelMapper.map(users, UserDto.class);
         }
         return userDto;
     }
+
     @SneakyThrows
-    @CachePut(cacheNames = "users",key = "#id")
-    public UserDto updateUserById(int id, UserDto userDto){
+    @CachePut(cacheNames = "users", key = "#id")
+    public UserDto updateUserById(int id, UserDto userDto) {
         Optional<UserEntity> users;
         UserEntity updatedUserEntity;
         users = userRepository.findById(id);
         UserEntity userEntity;
         log.info("UsersService::updateUser() connecting to Database");
-        if(users.isPresent()) {
+        if (users.isPresent()) {
             users = Optional.ofNullable(modelMapper.map(userDto, UserEntity.class));
             userEntity = users.get();
             userEntity.setId(id);
             updatedUserEntity = userRepository.save(userEntity);
             userDto = modelMapper.map(updatedUserEntity, UserDto.class);
-            kafkaTemplate.send(topicName,new ObjectMapper().writeValueAsString(userDto));
+            kafkaTemplate.send(topicName, new ObjectMapper().writeValueAsString(userDto));
             log.info("Published user update event to kafka user-event topic");
         }
         return userDto;
     }
+
     @SneakyThrows
-    @CacheEvict(cacheNames = "users",key = "#id")
-    public long deleteUsersById(int id){
+    @CacheEvict(cacheNames = "users", key = "#id")
+    public long deleteUsersById(int id) {
         Optional<UserEntity> users;
-        long count=0;
+        long count = 0;
         users = userRepository.findById(id);
         log.info("UsersService::deleteUsersById() connecting to Database");
-        if(users.isPresent()) {
+        if (users.isPresent()) {
             userRepository.deleteById(id);
-            count= userRepository.count();
-            kafkaTemplate.send(topicName,new ObjectMapper().writeValueAsString(users.get()));
+            count = userRepository.count();
+            kafkaTemplate.send(topicName, new ObjectMapper().writeValueAsString(users.get()));
             log.info("Published user delete event to kafka user-event topic");
         }
         return count;
