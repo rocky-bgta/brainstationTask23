89947a34ba3e12a265cd128a7813b13c16f13dd8
Salahin <rocky.bgta@gmail.com>
2023-12-17 15:20:33 +0600

    add kafka consumer

---
+++ src/main/java/com/salahin/rocky/kafka/consumer/UserEventConsumer.java
@@ -0,0 +1,25 @@
+package com.salahin.rocky.kafka.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserEventConsumer {
+
+    @KafkaListener(topics = "user-event", groupId = "user_event_consumer_group")
+    public void userEventConsumer1(String userEvent) {
+        log.info("userEventConsumer1 consumed message {} ", userEvent);
+    }
+
+    @KafkaListener(topics = "user-event", groupId = "user_event_consumer_group")
+    public void userEventConsumer2(String userEvent){
+        log.info("userEventConsumer2 consumed message {} ", userEvent);
+    }
+
+    @KafkaListener(topics = "user-event", groupId = "user_event_consumer_group")
+    public void userEventConsumer3(String userEvent){
+        log.info("userEventConsumer3 consumed message {} ", userEvent);
+    }
+}
