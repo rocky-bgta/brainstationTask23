a39bd0ed0204243c6de2cb94b10ace90384ca88e
Salahin <rocky.bgta@gmail.com>
2023-12-17 11:16:49 +0600

    init project

---
+++ src/main/java/com/salahin/rocky/SpringSeedApplication.java
@@ -0,0 +1,13 @@
+package com.salahin.rocky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salahin/rocky/SpringSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salahin.rocky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
